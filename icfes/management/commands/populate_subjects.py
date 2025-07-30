cf1ec1702ea99b59ae23bf26a8a693e5ca8617f2
YursKsf1 <yurs.ksf@gmail.com>
2025-07-29 19:03:18 -0500

    mejorar la carga automatica de registros para categoritas y materias

--- icfes/management/commands/populate_subjects.py
+++ icfes/management/commands/populate_subjects.py
@@ -4,14 +4,30 @@ from icfes.models import Subject, Category
 class Command(BaseCommand):
     def handle(self, *args, **options):
         print('Hola mundo')
-        subjects = ["Geometria", "Estadistica", "Aritmetica", "Algebra"]
+        #subjects = ["Geometria", "Estadistica", "Aritmetica", "Algebra"]
 
-        category = Category.objects.filter(name="Matematicas").first()
-        for subjec in subjects:
-            print(f'Creando la materia {subjec}')
-            sub = Subject(
-                name = subjec,
-                category = category
-            )
-            sub.save()
+        subjects = {
+            "Matematicas": ["Geometria", "Estadistica", "Aritmetica", "Algebra"],
+            "Ingles": ["Comprension Lectora", "Gramática", "Vocabulario"],
+            "Literatura": ["Análisis Literario", "Historia de la Literatura"],
+            "Sociales": ["Historia", "Geografía", "Cívica"],
+            "Biologia": ["Botánica", "Zoología", "Genética"]
+        }
+
+        for category_name, subjects in subjects.items():
+            category = Category.objects.filter(name=category_name).first()
+            if not category:
+                print(f'Categoria {category_name} no encontrada, creando una nueva.')
+                category = Category(name=category_name)
+                category.save()
+
+            for subjec in subjects:
+                print(f'Creando la materia {subjec}')
+                if not Subject.objects.filter(name=subjec, category=category).exists():
+                    print(f'Creando la materia {subjec} bajo la categoria {category_name}')
+                    sub = Subject(
+                        name=subjec,
+                        category=category
+                    )
+                    sub.save()
         print('Se han creado las materias exitosamente')
